8d63bd28a4070cc65f2264178465a2d6d3bd87e8
Brian Schwind <brianmschwind@gmail.com>
2024-02-18 23:02:59 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,155 @@
+# rp2040-i2s
+
+Read and write to I2S devices like MEMS microphones or DACs, on the RP2040 microcontroller.
+
+This crate currently assumes a fixed system clock at exactly 61440000 Hz, and uses a clock
+divider of exactly 10 to hit the required PIO clock to read and output 48 kHz audio. This
+assumption can be changed fairly easily by adding some flexibility in the code to accept
+a different PIO clock divider. Note: inexact (fractional) divisions of the system clock can
+lead to clock jitter, which may or may not effect the performance of your audio application.
+
+## Usage
+
+### I2S Output
+
+```rust
+let mut pac = pac::Peripherals::take().unwrap();
+let _core = pac::CorePeripherals::take().unwrap();
+
+let mut watchdog = Watchdog::new(pac.WATCHDOG);
+
+// Start Clocks
+// Found with: https://github.com/bschwind/rp2040-clock-calculator
+let desired_system_clock = 61440000.Hz();
+
+// Exercise for the reader
+let clocks = set_system_clock_exact(
+    desired_system_clock,
+    pac.XOSC,
+    pac.CLOCKS,
+    pac.PLL_SYS,
+    pac.PLL_USB,
+    &mut pac.RESETS,
+    &mut watchdog,
+)
+.unwrap();
+
+let sio = rp2040_hal::Sio::new(pac.SIO);
+
+let pins =
+    rp2040_hal::gpio::Pins::new(pac.IO_BANK0, pac.PADS_BANK0, sio.gpio_bank0, &mut pac.RESETS);
+
+// PIO Globals
+let (mut pio0, sm0, _, _, _) = pac.PIO0.split(&mut pac.RESETS);
+
+// I2S output
+let dac_output = I2SOutput::new(&mut pio0, sm0, pins.gpio6, pins.gpio7, pins.gpio8).unwrap();
+
+let (dac_sm, _dac_fifo_rx, dac_fifo_tx) = dac_output.split();
+
+// Write your audio samples to `dac_fifo_tx` in your favorite way (direct read or DMA transfer).
+```
+
+### I2S Input
+
+```rust
+let mut pac = pac::Peripherals::take().unwrap();
+let _core = pac::CorePeripherals::take().unwrap();
+
+let mut watchdog = Watchdog::new(pac.WATCHDOG);
+
+// Start Clocks
+// Found with: https://github.com/bschwind/rp2040-clock-calculator
+let desired_system_clock = 61440000.Hz();
+
+// Exercise for the reader
+let clocks = set_system_clock_exact(
+    desired_system_clock,
+    pac.XOSC,
+    pac.CLOCKS,
+    pac.PLL_SYS,
+    pac.PLL_USB,
+    &mut pac.RESETS,
+    &mut watchdog,
+)
+.unwrap();
+
+let sio = rp2040_hal::Sio::new(pac.SIO);
+
+let pins =
+    rp2040_hal::gpio::Pins::new(pac.IO_BANK0, pac.PADS_BANK0, sio.gpio_bank0, &mut pac.RESETS);
+
+// PIO Globals
+let (mut pio0, _, sm1, _, _) = pac.PIO0.split(&mut pac.RESETS);
+
+// I2S input
+let mic_input = I2SInput::new(&mut pio0, sm1, pins.gpio10, pins.gpio11, pins.gpio12).unwrap();
+
+let (mic_sm, mic_fifo_rx, _mic_fifo_tx) = mic_input.split();
+
+// Read mic audio samples from `mic_fifo_rx` in your favorite way (direct read or DMA transfer).
+```
+
+### I2S Multi-Input
+
+One PIO state machine can generate bit clock and word clock signals, while other PIO state
+machines can synchronize with the clock generator PIO and just read IS2 data at the proper
+timing. This reduces the number of pins required when interfacing with multiple microphones.
+
+```rust
+let mut pac = pac::Peripherals::take().unwrap();
+let _core = pac::CorePeripherals::take().unwrap();
+
+let mut watchdog = Watchdog::new(pac.WATCHDOG);
+
+// Start Clocks
+// Found with: https://github.com/bschwind/rp2040-clock-calculator
+let desired_system_clock = 61440000.Hz();
+
+// Exercise for the reader
+let clocks = set_system_clock_exact(
+    desired_system_clock,
+    pac.XOSC,
+    pac.CLOCKS,
+    pac.PLL_SYS,
+    pac.PLL_USB,
+    &mut pac.RESETS,
+    &mut watchdog,
+)
+.unwrap();
+
+let sio = rp2040_hal::Sio::new(pac.SIO);
+
+let pins =
+    rp2040_hal::gpio::Pins::new(pac.IO_BANK0, pac.PADS_BANK0, sio.gpio_bank0, &mut pac.RESETS);
+
+// PIO Globals
+let (mut pio0, _, sm1, sm2, _) = pac.PIO0.split(&mut pac.RESETS);
+
+// I2S input
+let mic_1_input = I2SInput::new(&mut pio0, sm1, pins.gpio10, pins.gpio11, pins.gpio12).unwrap();
+let mic_2_input = I2SInput::new_data_only(&mut pio0, sm2, pins.gpio15).unwrap();
+
+let (mic_1_sm, mic_1_fifo_rx, _mic_1_fifo_tx) = mic_1_input.split();
+let (mic_2_sm, mic_2_fifo_rx, _mic_2_fifo_tx) = mic_2_input.split();
+
+// Synchronize both PIO state machines so they start at the exact same time and
+// execute their instructions in lockstep.
+mic_sm.with(mic_2_sm).sync().start();
+
+// Receive mic data from `mic_1_fifo_rx` and `mic_2_fifo_rx`.
+```
+
+## Code Format
+
+The formatting options currently use nightly-only options.
+
+```
+$ cargo +nightly fmt
+```
+
+## Code Linting
+
+```
+$ cargo clippy --all-targets -- -D warnings
+```
---
+++ src/lib.rs
@@ -0,0 +1,228 @@
+#![no_std]
+
+use rp2040_hal::{
+    gpio::{FunctionNull, Pin, PinId, PullDown, ValidFunction},
+    pio::{
+        InstallError, PIOExt, Rx, StateMachine, StateMachineIndex, Stopped, Tx, UninitStateMachine,
+        ValidStateMachine, PIO,
+    },
+};
+
+#[derive(Debug)]
+pub enum I2SError {
+    PioInstallationError(InstallError),
+}
+
+pub trait SampleReader {
+    fn read(&mut self) -> Option<u32>;
+}
+
+impl<SM: ValidStateMachine> SampleReader for Rx<SM> {
+    fn read(&mut self) -> Option<u32> {
+        self.read()
+    }
+}
+
+pub struct I2SOutput<P: rp2040_hal::pio::PIOExt, SM: rp2040_hal::pio::StateMachineIndex> {
+    state_machine: StateMachine<(P, SM), Stopped>,
+    fifo_rx: Rx<(P, SM)>,
+    fifo_tx: Tx<(P, SM)>,
+}
+
+impl<P: PIOExt, SM: StateMachineIndex> I2SOutput<P, SM> {
+    /// Create an I2S output with a data line, a bit clock, and a left/right word clock.
+    /// The left/right word clock pin MUST consecutively follow the bit clock pin. So if
+    /// the bit clock pin is 7, the word clock pin MUST be 8.
+    pub fn new<DataPin, BitClockPin, LeftRightClockPin>(
+        pio: &mut PIO<P>,
+        state_machine: UninitStateMachine<(P, SM)>,
+        data_out_pin: Pin<DataPin, FunctionNull, PullDown>,
+        bit_clock_pin: Pin<BitClockPin, FunctionNull, PullDown>,
+        left_right_clock_pin: Pin<LeftRightClockPin, FunctionNull, PullDown>,
+    ) -> Result<Self, I2SError>
+    where
+        DataPin: PinId + ValidFunction<P::PinFunction>,
+        BitClockPin: PinId + ValidFunction<P::PinFunction>,
+        LeftRightClockPin: PinId + ValidFunction<P::PinFunction>,
+    {
+        let data_out_pin: Pin<_, P::PinFunction, _> = data_out_pin.into_function();
+        let bit_clock_pin: Pin<_, P::PinFunction, _> = bit_clock_pin.into_function();
+        let left_right_clock_pin: Pin<_, P::PinFunction, _> = left_right_clock_pin.into_function();
+
+        let data_pin_id = data_out_pin.id().num;
+        let bit_clock_pin_id = bit_clock_pin.id().num;
+        let left_right_clock_pin_id = left_right_clock_pin.id().num;
+
+        assert_eq!(
+            left_right_clock_pin_id - bit_clock_pin_id,
+            1,
+            "The word clock pin must consecutively follow the bit clock pin"
+        );
+
+        #[rustfmt::skip]
+        let dac_pio_program = pio_proc::pio_asm!(
+            ".side_set 2",
+            "    set x, 30          side 0b01", // side 0bWB - W = Word Clock, B = Bit Clock
+            "left_data:",
+            "    out pins, 1        side 0b00",
+            "    jmp x-- left_data  side 0b01",
+            "    out pins 1         side 0b10",
+            "    set x, 30          side 0b11",
+            "right_data:",
+            "    out pins 1         side 0b10",
+            "    jmp x-- right_data side 0b11",
+            "    out pins 1         side 0b00",
+        );
+
+        let installed =
+            pio.install(&dac_pio_program.program).map_err(I2SError::PioInstallationError)?;
+
+        let (mut dac_sm, fifo_rx, fifo_tx) = rp2040_hal::pio::PIOBuilder::from_program(installed)
+            .out_pins(data_pin_id, 1)
+            .side_set_pin_base(bit_clock_pin_id)
+            .out_shift_direction(rp2040_hal::pio::ShiftDirection::Left)
+            .clock_divisor_fixed_point(10, 0)
+            .buffers(rp2040_hal::pio::Buffers::OnlyTx)
+            .autopull(true)
+            .pull_threshold(32)
+            .build(state_machine);
+
+        dac_sm.set_pindirs([
+            (data_pin_id, rp2040_hal::pio::PinDir::Output),
+            (bit_clock_pin_id, rp2040_hal::pio::PinDir::Output),
+            (left_right_clock_pin_id, rp2040_hal::pio::PinDir::Output),
+        ]);
+
+        Ok(Self { state_machine: dac_sm, fifo_rx, fifo_tx })
+    }
+
+    #[allow(clippy::type_complexity)]
+    pub fn split(self) -> (StateMachine<(P, SM), Stopped>, Rx<(P, SM)>, Tx<(P, SM)>) {
+        (self.state_machine, self.fifo_rx, self.fifo_tx)
+    }
+}
+
+pub struct I2SInput<P: rp2040_hal::pio::PIOExt, SM: rp2040_hal::pio::StateMachineIndex> {
+    state_machine: StateMachine<(P, SM), Stopped>,
+    fifo_rx: Rx<(P, SM)>,
+    fifo_tx: Tx<(P, SM)>,
+}
+
+impl<P: PIOExt, SM: StateMachineIndex> I2SInput<P, SM> {
+    /// Create an I2S input with a data line, a bit clock, and a left/right word clock.
+    /// The left/right word clock pin MUST consecutively follow the bit clock pin. So if
+    /// the bit clock pin is 7, the word clock pin MUST be 8.
+    pub fn new<DataPin, BitClockPin, LeftRightClockPin>(
+        pio: &mut PIO<P>,
+        state_machine: UninitStateMachine<(P, SM)>,
+        data_in_pin: Pin<DataPin, FunctionNull, PullDown>,
+        bit_clock_pin: Pin<BitClockPin, FunctionNull, PullDown>,
+        left_right_clock_pin: Pin<LeftRightClockPin, FunctionNull, PullDown>,
+    ) -> Result<Self, I2SError>
+    where
+        DataPin: PinId + ValidFunction<P::PinFunction>,
+        BitClockPin: PinId + ValidFunction<P::PinFunction>,
+        LeftRightClockPin: PinId + ValidFunction<P::PinFunction>,
+    {
+        let data_in_pin: Pin<_, P::PinFunction, _> = data_in_pin.into_function();
+        let bit_clock_pin: Pin<_, P::PinFunction, _> = bit_clock_pin.into_function();
+        let left_right_clock_pin: Pin<_, P::PinFunction, _> = left_right_clock_pin.into_function();
+
+        let data_pin_id = data_in_pin.id().num;
+        let bit_clock_pin_id = bit_clock_pin.id().num;
+        let left_right_clock_pin_id = left_right_clock_pin.id().num;
+
+        assert_eq!(
+            left_right_clock_pin_id - bit_clock_pin_id,
+            1,
+            "The word clock pin must consecutively follow the bit clock pin"
+        );
+
+        #[rustfmt::skip]
+        let mic_pio_program = pio_proc::pio_asm!(
+            ".side_set 2",
+            "    set x, 30          side 0b00", // side 0bWB - W = Word Clock, B = Bit Clock
+            "left_data:",
+            "    in pins, 1         side 0b01",
+            "    jmp x-- left_data  side 0b00",
+            "    in pins, 1         side 0b11",
+            "    set x, 30          side 0b10",
+            "right_data:",
+            "    in pins, 1         side 0b11",
+            "    jmp x-- right_data side 0b10",
+            "    in pins, 1         side 0b01",
+        );
+
+        let installed = pio.install(&mic_pio_program.program).unwrap();
+
+        let (mut mic_sm, fifo_rx, fifo_tx) = rp2040_hal::pio::PIOBuilder::from_program(installed)
+            .in_pin_base(data_pin_id)
+            .side_set_pin_base(bit_clock_pin_id)
+            .in_shift_direction(rp2040_hal::pio::ShiftDirection::Left)
+            .clock_divisor_fixed_point(10, 0)
+            .buffers(rp2040_hal::pio::Buffers::OnlyRx)
+            .autopush(true)
+            .push_threshold(32)
+            .build(state_machine);
+
+        mic_sm.set_pindirs([
+            (data_pin_id, rp2040_hal::pio::PinDir::Input),
+            (bit_clock_pin_id, rp2040_hal::pio::PinDir::Output),
+            (left_right_clock_pin_id, rp2040_hal::pio::PinDir::Output),
+        ]);
+
+        Ok(Self { state_machine: mic_sm, fifo_rx, fifo_tx })
+    }
+
+    /// Create an I2S input which only reads from a data line. This assumes
+    /// the I2S device is clocked by another state machine returned from
+    /// `I2SInput::new`. The state machines must have their clock dividers
+    /// sychronized, and start at the exact same time. The PIO program code
+    /// must also stay in sync in terms of when reads occur, and the number
+    /// of instructions.
+    pub fn new_data_only<DataPin>(
+        pio: &mut PIO<P>,
+        state_machine: UninitStateMachine<(P, SM)>,
+        data_in_pin: Pin<DataPin, FunctionNull, PullDown>,
+    ) -> Result<Self, I2SError>
+    where
+        DataPin: PinId + ValidFunction<P::PinFunction>,
+    {
+        let data_in_pin: Pin<_, P::PinFunction, _> = data_in_pin.into_function();
+        let data_pin_id = data_in_pin.id().num;
+
+        #[rustfmt::skip]
+        let mic_pio_program = pio_proc::pio_asm!(
+            "    set x, 30",
+            "left_data:",
+            "    in pins, 1",
+            "    jmp x-- left_data",
+            "    in pins, 1",
+            "    set x, 30",
+            "right_data:",
+            "    in pins, 1",
+            "    jmp x-- right_data",
+            "    in pins, 1",
+        );
+
+        let installed = pio.install(&mic_pio_program.program).unwrap();
+
+        let (mut mic_sm, fifo_rx, fifo_tx) = rp2040_hal::pio::PIOBuilder::from_program(installed)
+            .in_pin_base(data_pin_id)
+            .in_shift_direction(rp2040_hal::pio::ShiftDirection::Left)
+            .clock_divisor_fixed_point(10, 0)
+            .buffers(rp2040_hal::pio::Buffers::OnlyRx)
+            .autopush(true)
+            .push_threshold(32)
+            .build(state_machine);
+
+        mic_sm.set_pindirs([(data_pin_id, rp2040_hal::pio::PinDir::Input)]);
+
+        Ok(Self { state_machine: mic_sm, fifo_rx, fifo_tx })
+    }
+
+    #[allow(clippy::type_complexity)]
+    pub fn split(self) -> (StateMachine<(P, SM), Stopped>, Rx<(P, SM)>, Tx<(P, SM)>) {
+        (self.state_machine, self.fifo_rx, self.fifo_tx)
+    }
+}
